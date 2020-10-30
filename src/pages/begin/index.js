a55aee65eca56dc572df096390545a2d15c1f5b4
daisyzou201 <2571825997@qq.com>
2020-10-30 14:06:32 +0800

    feat: 开始答题页面

---
+++ src/pages/begin/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css'
+
+function App(props) {
+  const beginInterview=()=>{
+     console.log('cccc')
+  }
+  return ( <div className='begin'>
+      <p className='title'> 即将开始视频答题 </p> 
+      <p className='desc'> 打开前置摄像头， 确保正对摄像头 </p> 
+      <div className='example'> 
+        <img src='./assets/images/example.png'/>
+      </div>
+      <div className='btn' onClick={beginInterview}>准备好了，立即开始</div>
+
+      </div>
+  )
+}
+
+export default App
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/home'
 import Step from './pages/step'
+import Begin from './pages/begin'
+import Question from './pages/question'
 
 const BasicRoute = () => (
   <HashRouter>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/step" component={Step}/>
+        <Route exact path="/begin" component={Begin}/>
+        <Route exact path="/question" component={Question}/>
       </Switch>
   </HashRouter>
 );
